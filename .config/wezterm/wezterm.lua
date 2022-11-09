7060921a5b12e99d1d4d4cf7b629c6b344ad4d98
Annenpolka <lancelbb@gmail.com>
2022-11-09 11:17:54 +0900

    update wezterm config

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,3 +1,27 @@
+---@diagnostic disable: unused-local
+local wezterm = require("wezterm")
+local os = require("os")
+local is_mac = wezterm.target_triple:find("darwin")
+local is_windows = wezterm.target_triple:find("windows")
+local is_linux = wezterm.target_triple:find("linux")
+
+local default_prog = { os.getenv("SHELL") }
+-- use WSL in Windows
+if is_windows then
+	default_prog = {
+		"wsl.exe",
+		--[[ "--distribution", "Ubuntu-20.04", ]]
+		"--exec",
+		"/bin/zsh",
+		"-l",
+	}
+end
+
 return {
+	default_prog = default_prog,
 	color_scheme = "Batman",
+	font = wezterm.font_with_fallback({
+		"Iosevka Term",
+		"Migmix 1M",
+	}),
 }
