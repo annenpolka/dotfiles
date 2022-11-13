1970bcee55eb09985bac875b4d9d75871828729e
Annenpolka <lancelbb@gmail.com>
2022-11-13 15:51:33 +0900

    update wezterm wsl config

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -6,22 +6,30 @@ local is_windows = wezterm.target_triple:find("windows")
 local is_linux = wezterm.target_triple:find("linux")
 
 local default_prog = { os.getenv("SHELL") }
--- use WSL in Windows
+local font = wezterm.font_with_fallback({
+	"Iosevka Term",
+	"Migmix 1M",
+})
+
+-- Windows-WSL setup
 if is_windows then
 	default_prog = {
 		"wsl.exe",
+		"~",
 		--[[ "--distribution", "Ubuntu-20.04", ]]
 		"--exec",
 		"/bin/zsh",
 		"-l",
 	}
+
+	font = wezterm.font_with_fallback({
+		"Iosevka NF",
+		"Migmix 1M",
+	})
 end
 
 return {
 	default_prog = default_prog,
 	color_scheme = "Batman",
-	font = wezterm.font_with_fallback({
-		"Iosevka Term",
-		"Migmix 1M",
-	}),
+	font = font,
 }
