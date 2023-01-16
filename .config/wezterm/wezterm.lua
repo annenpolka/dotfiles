b792ec8e348dea9959541cb682e5cc4e63c4bb3a
Annenpolka <lancelbb@gmail.com>
2023-01-16 10:31:43 +0900

    add keymap

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -19,9 +19,36 @@ end
 
 return {
 	default_prog = default_prog,
+	--  ╭──────────────────────────────────────────────────────────╮
+	--  │                        Appearance                        │
+	--  ╰──────────────────────────────────────────────────────────╯
 	color_scheme = "Batman",
 	font = wezterm.font_with_fallback({
 		"Iosevka Term",
 		"Migmix 1M",
 	}),
+	use_fancy_tab_bar = false,
+	--  ╭──────────────────────────────────────────────────────────╮
+	--  │                          Keymap                          │
+	--  ╰──────────────────────────────────────────────────────────╯
+	-- timeout_milliseconds defaults to 1000 and can be omitted
+	leader = { key = "a", mods = "CTRL", timeout_milliseconds = 1000 },
+	keys = {
+		{
+			key = "\\",
+			mods = "LEADER",
+			action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+		},
+		{
+			key = "-",
+			mods = "LEADER",
+			action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
+		},
+		-- Send "CTRL-A" to the terminal when pressing CTRL-A, CTRL-A
+		{
+			key = "a",
+			mods = "LEADER|CTRL",
+			action = wezterm.action.SendString("\x01"),
+		},
+	},
 }
